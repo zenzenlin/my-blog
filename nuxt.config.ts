2ec084120bab2a9f38105d1da09da76710dde58d
zenzenlin <poietgr0107@gmail.com>
2025-05-04 15:05:10 +0800

    feat(config): Add .npmrc for dependency management, update deploy settings.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/content"],
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/"], // 額外手動列出要預產出的頁面
+    },
+  },
   app: {
     head: {
       htmlAttrs: {
@@ -10,5 +16,6 @@ export default defineNuxtConfig({
       },
       title: "zenzen 的部落格網站",
     },
+    baseURL: process.env.NODE_ENV === "production" ? "/my-blog/" : "/",
   },
 });
