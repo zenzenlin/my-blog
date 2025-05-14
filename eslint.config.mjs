e7fa8435b458c38da1edafc874a92a8cf7b2c946
zenzenlin <mike.lin@staff.nueip.com>
2025-05-14 18:13:34 +0800

    feat: eslint add rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always", // 允許 <input />、<br /> 等自閉合
+          normal: "always",
+          component: "always",
+        },
+        svg: "always",
+        math: "always",
+      },
+    ],
+  },
+});
