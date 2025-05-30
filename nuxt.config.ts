8aad897d57340fc8121cac728c973f2d5c382290
zenzenlin <poietgr0107@gmail.com>
2025-05-30 12:53:05 +0800

    feat: meta property

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,34 @@ export default defineNuxtConfig({
       },
       title: "zenzen 的部落格網站",
       link: [{ rel: "icon", type: "image/x-icon", href: "./favicon.ico" }],
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content:
+            "Frontend development, Vue, self-learning, focusing on AI, and data analysis exploration, supporting web design and career transition.",
+        },
+        {
+          name: "apple-mobile-web-app-status-bar-style",
+          content: "black-translucent",
+        },
+        {
+          name: "theme-color",
+          media: "(prefers-color-scheme: light)",
+          content: "white",
+        },
+        {
+          name: "theme-color",
+          media: "(prefers-color-scheme: dark)",
+          content: "black",
+        },
+        { name: "referrer", content: "no-referrer-when-downgrade" },
+        { name: "author", content: "Mike Lin" },
+        { name: "keywords", content: "前端開發、Vue、Coding" },
+        { name: "application-name", content: "Mike" },
+        { name: "apple-mobile-web-app-title", content: "Mike" },
+        { name: "apple-mobile-web-app-status-bar-style", content: "default" },
+      ],
     },
     baseURL: process.env.NODE_ENV === "production" ? "/my-blog/" : "/",
   },
